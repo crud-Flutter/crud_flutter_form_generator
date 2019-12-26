8b22f1ad0af7bfd32409f07afcaac4669b893b6b
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2019-12-26 11:27:57 -0300

    initial value form in entity edit

--- lib/src/form_state_flutter_generator.dart
+++ lib/src/form_state_flutter_generator.dart
@@ -16,28 +16,159 @@ class FormStateFlutterGenerator
     this.annotation = annotation;
     extend = refer('State<${element.name}FormPage>');
     _declareField();
+    _declareConstructor();
+    _methodInitState();
     _methodBuild();
     _methodSave();
+    _methodDateTimeFormat();
     return "import 'package:rxdart/subjects.dart';"
             "import '${element.name.toLowerCase()}.bloc.dart';"
             "import '${element.name.toLowerCase()}.form.stateful.dart';"
             "import 'package:flutter/services.dart';"
+            "import '${element.name.toLowerCase()}.entity.dart';"
             "import 'package:intl/intl.dart';" +
         build();
+  }
+
+  _methodDateTimeFormat() {
+    var blockBuilder = BlockBuilder()
+      ..statements.addAll([
+        Code('var dateFormat;'),
+        Code('if (context == null) {'),
+        Code('dateFormat = DateFormat.yMMMMd();'),
+        Code('}'),
+        Code('else {'),
+        Code(
+            'dateFormat = DateFormat.yMMMMd(Localizations.localeOf(context).languageCode);'),
+        Code('}'),
+        Code('return dateFormat.add_Hm().format(dateTime);')
+      ]);
+
+    declareMethod('dateTimeFormat',
+        returns: refer('String'),
+        requiredParameters: [
+          Parameter((b) => b
+            ..name = 'dateTime'
+            ..type = refer('DateTime'))
+        ],
+        body: blockBuilder.build());
   }
 
   _declareField() {
     declareField(refer('final'), '_formKey',
         assignment: Code('GlobalKey<FormState>()'));
     elementAsClass.fields.forEach((field) {
-      if (field.type.name == 'DateTime') {
-        declareField(refer('final'), '${field.name}Controller',
-            assignment: Code('TextEditingController()'));
-      }
+      declareField(refer('final'), '${field.name}Controller',
+          assignment: Code('TextEditingController()'));
+      declareField(refer('TextFormField'), field.name);
     });
     declareField(refer('${element.name}Bloc'), '_bloc',
         assignment: Code('${element.name}Bloc()'));
     declareField(refer('BuildContext'), 'context');
+    declareField(refer('$entityClass'), '$entityInstance');
+  }
+
+  _declareConstructor() {
+    declareConstructor(optionalParameters: [
+      Parameter((b) => b
+        ..name = 'this.${element.name.toLowerCase()}Entity'
+        ..named = true)
+    ]);
+  }
+
+  _methodInitState() {
+    var initStateCode = [Code('super.initState();')];
+    elementAsClass.fields.forEach((field) {
+      if (field.type.name == 'String') {
+        initStateCode.add(_variableTextField(field.name));
+      } else {
+        initStateCode
+            .add(_variableTextField(field.name, type: field.type.name));
+      }
+    });
+    initStateCode.add(Code('if ($entityInstance != null) {'));
+    initStateCode.add(Code('_bloc.set$entityClass($entityInstance);'));
+    elementAsClass.fields.forEach((field) {
+      if (field.type.name == 'String') {
+        initStateCode.add(Code(
+            '${field.name}Controller.text = $entityInstance.${field.name};'));
+      } else if (field.type.name == 'int' ||
+          field.type.name == 'double' ||
+          field.type.name == 'num') {
+        initStateCode.add(Code(
+            '${field.name}Controller.text = $entityInstance.${field.name} as String;'));
+      } else if (field.type.name == 'DateTime') {
+        initStateCode.add(Code('${field.name}Controller.text = dateTimeFormat($entityInstance.${field.name});'));
+      }
+    });
+    initStateCode.add(Code('}'));
+    var blockBuilder = BlockBuilder()..statements.addAll(initStateCode);
+    declareMethod('initState', body: blockBuilder.build());
+  }
+
+  Code _variableTextField(String name, {String type = 'String'}) {
+    var textFieldCode = [
+      Code('$name = TextFormField('),
+      Code(
+          "decoration: InputDecoration(hintText: '${name[0].toUpperCase()}${name.substring(1)}'),"),
+      Code('controller: ${name}Controller,'),
+    ];
+    var onChanged = Code('onChanged: _bloc.set${name},');
+    if (type == 'int' || type == 'num' || type == 'number') {
+      textFieldCode.add(Code('keyboardType: TextInputType.number,'));
+      if (type == 'int') {
+        textFieldCode.add(Code(
+            'inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],'));
+      }
+      onChanged =
+          Code('onChanged: (text) {_bloc.set${name}($type.parse(text));},');
+    }
+    if (type == 'DateTime') {
+      onChanged = _onChangedDateTimeField(name);
+      textFieldCode.add(Code('keyboardType: TextInputType.datetime,'));
+    }
+    textFieldCode.add(onChanged);
+    textFieldCode.add(Code(');'));
+    var blockBuilder = BlockBuilder()..statements.addAll(textFieldCode);
+    return blockBuilder.build();
+  }
+
+  _onChangedDateTimeField(String name) {
+    var onChangedCode = List<Code>();
+    onChangedCode.add(Code('onTap: () async {'));
+    onChangedCode
+        .add(Code('FocusScope.of(context).requestFocus(new FocusNode());'));
+    onChangedCode.add(Code('var selectedDate = await showDatePicker('));
+    onChangedCode.add(Code('context: context,'));
+    onChangedCode.add(Code(
+        'initialDate: (_bloc.out$name as BehaviorSubject).value ?? DateTime.now(),'));
+    onChangedCode.add(Code('firstDate: DateTime(2018),'));
+    onChangedCode.add(Code('lastDate: DateTime(2030),'));
+    onChangedCode.add(Code(');'));
+    onChangedCode.add(Code('if(selectedDate != null) {'));
+    onChangedCode.add(
+        Code('var initialTime = (_bloc.out$name as BehaviorSubject).value;'));
+    onChangedCode.add(Code('if (initialTime==null) {'));
+    onChangedCode.add(Code('initialTime = TimeOfDay.now();'));
+    onChangedCode.add(Code('} else {'));
+    onChangedCode
+        .add(Code('initialTime = TimeOfDay.fromDateTime(initialTime);'));
+    onChangedCode.add(Code('}'));
+    onChangedCode.add(Code('var selectedTime = await showTimePicker('));
+    onChangedCode.add(Code('context: context,'));
+    onChangedCode.add(Code('initialTime: initialTime,'));
+    onChangedCode.add(Code(');'));
+    onChangedCode.add(Code('if (selectedTime != null) {'));
+    onChangedCode.add(Code(
+        'var dateTime = DateTime(selectedDate.year, selectedDate.month, selectedDate.day, selectedTime.hour, selectedTime.minute);'));
+    onChangedCode
+        .add(Code('${name}Controller.text = dateTimeFormat(dateTime);'));
+    onChangedCode.add(Code('_bloc.set${name}(dateTime);'));
+    onChangedCode.add(Code('}'));
+    onChangedCode.add(Code('}'));
+    onChangedCode.add(Code('},'));
+    var blockBuilder = BlockBuilder()..statements.addAll(onChangedCode);
+    return blockBuilder.build();
   }
 
   _methodSave() {
@@ -62,69 +193,7 @@ class FormStateFlutterGenerator
     buildCode.add(Code('child: Column('));
     buildCode.add(Code('children:  <Widget> ['));
     elementAsClass.fields.forEach((field) {
-      buildCode.add(Code('TextFormField('));
-      if (field.type.name == 'int') {
-        buildCode.add(Code('keyboardType: TextInputType.number,'));
-        buildCode.add(Code(
-            'inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],'));
-        buildCode.add(Code('onChanged: (text) {'));
-        buildCode.add(Code('_bloc.set${field.name}(int.parse(text));'));
-        buildCode.add(Code('},'));
-      } else if (field.type.name == 'double') {
-        buildCode.add(Code('keyboardType: TextInputType.number,'));
-        buildCode.add(Code('onChanged: (text) {'));
-        buildCode.add(Code('_bloc.set${field.name}(double.parse(text));'));
-        buildCode.add(Code('},'));
-      } else if (field.type.name == 'num') {
-        buildCode.add(Code('keyboardType: TextInputType.number,'));
-        buildCode.add(Code('onChanged: (text) {'));
-        buildCode.add(Code('_bloc.set${field.name}(num.parse(text));'));
-        buildCode.add(Code('},'));
-      } else if (field.type.name == 'DateTime') {
-        buildCode.add(Code('keyboardType: TextInputType.datetime,'));
-        buildCode.add(Code('controller: ${field.name}Controller,'));
-        buildCode.add(Code('onTap: () async {'));
-        buildCode
-            .add(Code('FocusScope.of(context).requestFocus(new FocusNode());'));
-        buildCode.add(Code('var selectedDate = await showDatePicker('));
-        buildCode.add(Code('context: context,'));
-        buildCode.add(Code(
-            'initialDate: (_bloc.outdateCreation as BehaviorSubject).value ?? DateTime.now(),'));
-        buildCode.add(Code('firstDate: DateTime(2018),'));
-        buildCode.add(Code('lastDate: DateTime(2030),'));
-        buildCode.add(Code(');'));
-        buildCode.add(Code('if(selectedDate != null) {'));
-        buildCode.add(Code(
-            'var initialTime = (_bloc.outdateCreation as BehaviorSubject).value;'));
-        buildCode.add(Code('if (initialTime==null) {'));
-        buildCode.add(Code('initialTime = TimeOfDay.now();'));
-        buildCode.add(Code('} else {'));
-        buildCode
-            .add(Code('initialTime = TimeOfDay.fromDateTime(initialTime);'));
-        buildCode.add(Code('}'));
-        buildCode.add(Code('var selectedTime = await showTimePicker('));
-        buildCode.add(Code('context: context,'));
-        buildCode.add(Code('initialTime: initialTime,'));
-        buildCode.add(Code(');'));
-        buildCode.add(Code('if (selectedTime != null) {'));
-        buildCode.add(Code(
-            'var dateTime = DateTime(selectedDate.year, selectedDate.month, selectedDate.day, selectedTime.hour, selectedTime.minute);'));
-        buildCode.add(Code(
-            '${field.name}Controller.text = DateFormat.yMMMMd(Localizations.localeOf(context).languageCode).add_Hm().format(dateTime);'));
-        buildCode.add(Code('_bloc.set${field.name}(dateTime);'));
-        buildCode.add(Code('}'));
-        buildCode.add(Code('}'));
-        buildCode.add(Code('},'));
-      } else if (field.type.name == 'String') {
-        buildCode.add(Code('onChanged: _bloc.set${field.name},'));
-      }
-      buildCode.add(Code('decoration: InputDecoration('));
-
-      buildCode.add(Code(
-          "hintText: '${field.name[0].toUpperCase()}${field.name.substring(1)}'"));
-      buildCode.add(Code('),'));
-      // buildCode.add(Code('controller: ${field.name}Controller'));
-      buildCode.add(Code('),'));
+      buildCode.add(Code('${field.name},'));
     });
     buildCode.add(Code('])),'));
     var blockBuilder = BlockBuilder();
