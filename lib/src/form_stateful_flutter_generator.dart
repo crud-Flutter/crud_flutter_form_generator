fd7165bf8c6ef5a666908d121bb2a9229435cd2b
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2020-02-25 15:26:42 -0300

    Generation of methods by parts and many to many generation

--- lib/src/form_stateful_flutter_generator.dart
+++ lib/src/form_stateful_flutter_generator.dart
@@ -1,48 +1,46 @@
-import 'package:analyzer/dart/element/element.dart';
-import 'package:build/src/builder/build_step.dart';
 import 'package:code_builder/code_builder.dart';
 import 'package:crud_flutter_form_generator/crud_flutter_form_generator.dart';
 import 'package:crud_generator/crud_generator.dart';
-import 'package:source_gen/src/constants/reader.dart';
 
 class FormStatefulFlutterGenerator
     extends GenerateFlutterWidgetForAnnotation<FormEntity> {
   @override
-  generateForAnnotatedElement(
-      Element element, ConstantReader annotation, BuildStep buildStep) {
-    init();
-    name = '${element.name}FormPage';
-    this.element = element;
-    this.annotation = annotation;
+  String generateName() => '${element.name}${manyToManyPosFix}FormPage';
+  @override
+  void optionalClassInfo() {
     extend = refer('StatefulWidget');
     addImportPackage('package:flutter/material.dart');
-    _declareField();
-    _declareConstructor();
-    _methodCreateState();
-    return build();
   }
 
-  _declareField() {
-    addImportPackage('${element.name.toLowerCase()}.entity.dart');
-    declareField(
-        refer('${element.name}Entity'), '${element.name.toLowerCase()}Entity',
-        modifier: FieldModifier.final$);
+  void generateFields() {}
+
+  generateConstructors() {
+    var parameters = List<Parameter>();
+    if (manyToMany) {
+      parameters = [
+        Parameter((b) => b
+          ..name = 'collectionParent'
+          ..type = refer('String')),
+        Parameter((b) => b
+          ..name = 'documentId'
+          ..type = refer('String'))
+      ];
+    }
+    declareConstructor(optionalParameters: parameters);
   }
 
-  _declareConstructor() {
-    declareConstructor(optionalParameters: [
-      Parameter((b) => b
-        ..name = 'this.${element.name.toLowerCase()}Entity'
-        ..named = true)
-    ]);
-  }
+  void generateMethods() {
+    if (!manyToMany)
+      addImportPackage('${element.name.toLowerCase()}.form.state.dart');
 
-  void _methodCreateState() {
-    addImportPackage('${element.name.toLowerCase()}.form.state.dart');
     declareMethod('createState',
-        returns: refer('${element.name}FormPageState'),
+        returns: refer('${element.name}${manyToManyPosFix}FormPageState'),
         lambda: true,
-        body: Code(
-            '${element.name}FormPageState(${element.name.toLowerCase()}Entity: ${element.name.toLowerCase()}Entity)'));
+        body: Code('${element.name}${manyToManyPosFix}FormPageState()'));
   }
+
+  @override
+  GenerateClassForAnnotation instance() => FormStatefulFlutterGenerator()
+    ..manyToMany = true
+    ..generateImport = false;
 }
